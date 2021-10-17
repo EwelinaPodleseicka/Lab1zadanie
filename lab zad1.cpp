4d808ec47ddac8105d34eb75ce2051f279158b42
EwelinaPodleseicka <92682631+EwelinaPodleseicka@users.noreply.github.com>
2021-10-17 17:27:40 +0200

    Add files via upload

---
+++ lab zad1.cpp
@@ -0,0 +1,125 @@
+﻿
+#include <iostream>
+using namespace std;
+//zad1
+//
+//int main()
+//{
+//	//ctrl+k+c- komentowanie ctrl+k+c+u - odkomentowanie kodu
+//	int nr_albumu = 65563;
+//	string nr_albumu1 = "w65563";
+//	cout << " Nr albumu " << nr_albumu << endl;
+//	cout << " Nr albumu " << nr_albumu1 << endl;
+//
+//zad2
+// 
+//	int a, b;//deklaracja zmiennych
+//	//wczytywanie danych z klawiatury
+//	cout << "Podaj a: ", cin >> a;
+//	//count << "Podaj b:", cin>> b 
+//
+//	if (a % 2 == 0) {
+//		cout << "liczba parzysta\n";
+//		cout << "kolejna linia tekstu\n";
+//	}
+//
+//	else
+//		cout << "liczba nie parzysta" << endl;
+//	}
+//zad3
+// 
+//int main()
+//{
+//	float a, b;//deklaracja zmiennych
+//	//wczytywanie danych
+//	cout << "Podaj a: ", cin >> a;
+//	cout << "Podaj b:", cin >> b;
+//
+//	if (a == b)
+//		cout << "Liczby są rowne \n";
+//
+//	else if (a > b)
+//		cout << "a jest wieksze" << endl;
+//	else
+//		cout << "b jest wieksze\n";
+//}
+//zad5
+
+
+/*int main()
+{
+	float a = 23, b = 2;
+	int wybor;
+	cout << "n==================\n";
+	cout << "\n1. Suma\n2. Roznica\,3. iloczyn\n4. Iloraz";
+	cout << "Wybierz dzialanie", cin >> wybor;
+	switch (wybor) {
+	case 1:
+		cout << "Suma: " << a << "+" << b << "=" << a + b << endl;
+		break;
+	case 2:
+		cout << "Roznica" << a << "-" << b << "=" << a - b << endl;
+		break;
+	case 3:
+		cout << "Iloczyn" << a << "*" << b << "=" << a * b << endl;
+		break;
+	case 4:
+		cout << "Iloczyn" << a << "/" << b << "=" << a / b << endl;
+		break;
+	default:
+		cout << "Nie ma takiej opcji\n";
+		break;
+	}
+}
+*/
+
+
+
+
+
+//switch (warunek)
+//{
+//case 1:
+//	//instrukcje 
+//	break;
+//case 2:
+//	//instrukcje
+//	break;
+//case 3:
+//	//instrukcje 
+//	break;
+//default:
+//	break :
+//}
+
+ //zadanie4
+/*int main() {
+
+
+	float a, b, c, d, srednia;
+	std::cout << "Podaj liczbe a ";
+	std::cin >> a;
+		std::cout << "Podaj liczbe b ";
+		std::cin >> b;
+		std::cout << "Podaj liczbe c ";
+		std::cin >> c;
+		std::cout << "Podaj liczbe d ";
+		std::cin >> d;
+srednia = (a + b + c + d) / 4;
+std::cout << "Srednia arytmeyczna liczb to = " << srednia << endl;
+return 0;
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
