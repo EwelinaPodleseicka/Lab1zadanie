44327f2eb9516384763883bcdc686bca57def3cf
EwelinaPodleseicka <92682631+EwelinaPodleseicka@users.noreply.github.com>
2021-12-21 18:06:04 +0100

    Add files via upload

---
+++ Labb6.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    ////zad1
+    //int r, polek, objetosck;
+    //string wybor;
+    //cin >> wybor;
+    //cout << " Podaj dlugosc promienia : "; cin >> r;
+    //cout << "Dlugosc promienia wynosi:" << r << endl;
+    //cout << "Co chcesz obliczyc? Napisz objetosck lub polek:";
+    //if (cin >> objetosck)
+    //{
+    //    objetosck = 2 * 3.14 * r;
+    //    cout << "Ojetosc wynosi:" << objetosck;
+    //}
+    //else if (cin >> polek)
+    //{
+    //    polek =  3.14 *( r * r);
+    //    cout << "Pole wynosi:" << polek;
+    //}h
+
+    //zad2
+    int liczba;
+    cin >> liczba;
+
+    string wynik;
+
+    while (liczba) {
+        wynik = (liczba % 2 ? "1" : "0") + wynik;
+        liczba /= 2;
+    }
+    cout << wynik;
+
+    //zad4
+    int dwusilnia(int n) {
+        if (n == 0) return 1;
+        if (n == 2) return 2;
+        if (n == 1) return 1;
+        return n * dwusilnia(n - 2);
+    }
+
+
+    //zad3
+    bool LiczbaPierwsza(int Liczba) {
+        if (Liczba == 1) return false;
+        if (Liczba == 2) return true;
+        if (Liczba % 2 == 0) return false;
+        for (int i = 3; i * i <= Liczba; i += 2) {
+            if (Liczba % i == 0) return false;
+        }
+        return true;
+    }
+
+   
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
